1093281da79400263c9d15d187f588f87021232c
Dan Kennedy <dannykennedy@email.com>
2020-01-14 18:44:38 +1100

    import rdflib for storing graph in memory

--- graphsby.py
+++ graphsby.py
@@ -1,6 +1,53 @@
 #!/usr/local/bin/python3
 
-import markdown2, os, re
+import markdown2, os, re, rdflib
+from rdflib import Namespace
+
+# Namespace declarations
+# RDFlib uses square brackets to show prefix and literals
+owlNS = Namespace("http://www.w3.org/2002/07/owl#")
+owlClass = owlNS["Class"]
+owlObjectProperty = owlNS["ObjectProperty"]
+owlDatatypeProperty = owlNS["DatatypeProperty"]
+rdfNS = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfProperty = rdfNS["Property"]
+rdfType = rdfNS["type"]
+rdfsNS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+rdfsSubClassOf = rdfsNS["subClassOf"]
+rdfsDomain = rdfsNS["domain"]
+rdfsRange = rdfsNS["range"]
+xsdNS = Namespace("http://www.w3.org/2001/XMLSchema#")
+xsdString = xsdNS["string"]
+dreamNS = Namespace("https://www.dannykennedy.co/dnj-ontology#")
+
+# Class names
+# (Start with capitals)
+personClass = dreamNS['Person']
+pageClass = dreamNS['Page']
+thingClass = dreamNS['Thing']
+placeClass = dreamNS['Place']
+
+# Property names start with lowercase
+name = dreamNS['name']
+tag = dreamNS['tag']
+orderContains = dreamNS['orderContains']
+availableAt = dreamNS['availableAt']
+
+# "object is a class in our ontology"
+classTriples = [
+	(dreamNS['Person'], rdfType, owlClass),
+	(dreamNS['Page'], rdfType, owlClass),
+	(dreamNS['Thing'], rdfType, owlClass),
+	(dreamNS['Place'], rdfType, owlClass)
+]
+
+
+
+
+
+
+
+
 
 rootdir = os.getcwd() + '/_items'
 
@@ -24,3 +71,5 @@ for subdir, dirs, files in os.walk(rootdir):
 		newfile = open(writepath, "w")
 		newfile.write(htmlstring)
 		newfile.close()
+		
+		

268aa5889871225823a6cadd94bc9a5599b8753e
Dan Kennedy <dannykennedy@email.com>
2020-01-19 18:12:30 +1100

    basic working ontology, outputs Turtle file

--- graphsby.py
+++ graphsby.py
@@ -16,30 +16,145 @@ rdfsNS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 rdfsSubClassOf = rdfsNS["subClassOf"]
 rdfsDomain = rdfsNS["domain"]
 rdfsRange = rdfsNS["range"]
-xsdNS = Namespace("http://www.w3.org/2001/XMLSchema#")
-xsdString = xsdNS["string"]
 dreamNS = Namespace("https://www.dannykennedy.co/dnj-ontology#")
+
+# XML properties
+xsdNS = Namespace("http://www.w3.org/2001/XMLSchema#")
+xsdString = xsdNS["string"] #xsd:string
+xsdDateTime = xsdNS["dateTime"] #xsd:dateTime
+xsdInteger = xsdNS["integer"] #xsd:integer
+xsdDecimal = xsdNS["decimal"] #xsd:decimal
 
 # Class names
 # (Start with capitals)
 personClass = dreamNS['Person']
+userClass = dreamNS['User']
 pageClass = dreamNS['Page']
-thingClass = dreamNS['Thing']
+actorClass = dreamNS['Actor']
+itemClass = dreamNS['Item']
+otherItemClass = dreamNS['OtherItem']
 placeClass = dreamNS['Place']
+postClass = dreamNS['Post']
+imageClass = dreamNS['Image']
 
-# Property names start with lowercase
+# Object properties
+hasTag = dreamNS['hasTag']
+hasAuthor = dreamNS['hasAuthor']
+featuredImage = dreamNS['featuredImage']
+
+# Datatype properties
 name = dreamNS['name']
-tag = dreamNS['tag']
-orderContains = dreamNS['orderContains']
-availableAt = dreamNS['availableAt']
+description = dreamNS['description']
+itemId = dreamNS['itemId']
+handle = dreamNS['handle']
+dateCreated = dreamNS['dateCreated']
 
 # "object is a class in our ontology"
 classTriples = [
-	(dreamNS['Person'], rdfType, owlClass),
-	(dreamNS['Page'], rdfType, owlClass),
-	(dreamNS['Thing'], rdfType, owlClass),
-	(dreamNS['Place'], rdfType, owlClass)
+	(personClass, rdfType, owlClass),
+	(otherItemClass, rdfType, owlClass),
+	(pageClass, rdfType, owlClass),
+	(actorClass, rdfType, owlClass),
+	(itemClass, rdfType, owlClass),
+	(placeClass, rdfType, owlClass),
+	(postClass, rdfType, owlClass),
+	(imageClass, rdfType, owlClass)
 ]
+
+classHierarchyTriples = [
+	# items
+	(actorClass, rdfsSubClassOf, itemClass),
+	(userClass, rdfsSubClassOf, personClass),
+	(userClass, rdfsSubClassOf, actorClass),
+	(pageClass, rdfsSubClassOf, actorClass),
+	(otherItemClass, rdfsSubClassOf, itemClass),
+	(personClass, rdfsSubClassOf, itemClass),
+	(placeClass, rdfsSubClassOf, itemClass),
+	(postClass, rdfsSubClassOf, itemClass),
+	(imageClass, rdfsSubClassOf, postClass),
+]
+
+propertyTriples = [
+	# DATATYPE PROPERTIES
+
+	# id (everything has an integer ID - timestamp + random number)
+	(itemId, rdfType, owlDatatypeProperty),
+	(itemId, rdfsDomain, itemClass),
+	(itemId, rdfsRange, xsdInteger),
+
+	# name (could be the name of a person, the title of a post, etc)
+	(name, rdfType, owlDatatypeProperty),
+	(name, rdfsDomain, itemClass),
+	(name, rdfsRange, xsdString),
+
+	# handle (unique string, only for users and pages. Prefixed with "@")
+	(handle, rdfType, owlDatatypeProperty),
+	(handle, rdfsDomain, actorClass),
+	(handle, rdfsRange, xsdString),
+
+	# description (could be the text of a post, or the blurb for a person)
+	(description, rdfType, owlDatatypeProperty),
+	(description, rdfsDomain, itemClass),
+	(description, rdfsRange, xsdString),
+
+	(dateCreated, rdfType, owlDatatypeProperty),
+	(dateCreated, rdfsDomain, postClass),
+	(dateCreated, rdfsRange, xsdDateTime),
+
+	# OBJECT PROPERTIES
+
+	# Tag
+	(hasTag, rdfType, owlObjectProperty),
+	(hasTag, rdfsDomain, itemClass),
+	(hasTag, rdfsRange, itemClass),
+
+	# Author
+	(hasAuthor, rdfType, owlObjectProperty),
+	(hasAuthor, rdfsDomain, personClass),
+	(hasAuthor, rdfsRange, postClass),
+]
+
+
+
+from rdflib import ConjunctiveGraph
+graph = ConjunctiveGraph()
+
+for triple in classTriples:
+	graph.add(triple)
+
+for triple in classHierarchyTriples:
+	graph.add(triple)
+
+for triple in propertyTriples:
+	graph.add(triple)
+
+
+
+################
+# OUTPUT GRAPH
+################
+
+graph.serialize(destination='dream-network9.ttl', format='turtle')
+
+
+
+
+
+
+
+
+
+################
+# INSTANCE DATA
+################
+
+# When you find an item, store it by its ID
+itemId = 'b3eae388-e8d2-4834-8284-a170aef489f3'
+newitem = dreamNS[itemId]
+
+
+# Add properties to the item
+
 
 
 

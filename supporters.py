0f3d0f72f6cea6e450504cffcc004e01fa5c67f1
Dan Kennedy <dannykennedy@email.com>
2023-12-31 10:47:56 +1100

    feat: waggoner article first lucid dreams

--- supporters.py
+++ supporters.py
@@ -1,13 +1,13 @@
 supporters = [
 	{
 		'name': 'Elsewhere Dream Journal',
-		'description': 'A beautiful home for your dreams.',
+		'description': 'A beautiful home for your dreams',
 		'profileImg': 'elsewhere-logo-zoomed.png',
 		'url': 'https://elsewhere.to'
 	},
 	{
 		'name': "EastWest Works",
-		'description': 'Online dream group, in-person taiji & more, NYC.',
+		'description': 'Dream group, taiji & more, NYC',
 		'profileImg': 'eastwestworks-square.jpg',
 		'url': 'https://eastwest.works/Home.html'
 	}

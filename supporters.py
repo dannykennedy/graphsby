20fb450f806fb4715720afde73c63a9a941d2483
Dan Kennedy <dannykennedy@email.com>
2024-07-23 15:35:14 -0700

    feat: links to Jung page

--- supporters.py
+++ supporters.py
@@ -35,7 +35,27 @@ ullman_supporters = [
 
 ]
 
+jung_supporters = [
+	{
+		'name': 'Free Jungian-style dream interpretation',
+		'description': 'Try it for yourself, powered by AI from Elsewhere',
+		'descriptionB': 'Understand your dreams',
+		'profileImg': 'elsewhere-moon.jpg',
+		'url': 'https://elsewhere.to/interpretation/jungian/en/?src=dnj',
+		'target': '_blank'
+	},
+	{
+		'name': "EastWest Works",
+		'description': 'Dream group, taiji & more, NYC',
+		'profileImg': 'eastwestworks-square.jpg',
+		'url': 'https://eastwest.works/Home.html/?src=dnj',
+		'target': '_blank'
+	}
+
+]
+
 supporters_by_page_id = {
+	# Ullman
 	'bcpov6rssull': ullman_supporters,
 	'bcpoz4zmwwmu': ullman_supporters,
 	'bcpox5dadaga': ullman_supporters,
@@ -45,4 +65,23 @@ supporters_by_page_id = {
 	'bcpov6zdbrvd': ullman_supporters,
 	'bcpoz6zpttmu': ullman_supporters,
 	'bcpoy2htdfsa': ullman_supporters,
+	# Jung
+	'bcpov6zfgadm': jung_supporters,
+	'bcpov6zfredb': jung_supporters,
+	'bcpov6zpitrb': jung_supporters,
+	'bcsoz6zinmiw': jung_supporters,
+	'bcpov6knenww': jung_supporters,
+	'bcpov6rsppm1': jung_supporters,
+	'bcpov3zpihdp': jung_supporters,
+	'bcpov6mhilli': jung_supporters,
+	'bcpov6zfgil1': jung_supporters,
+	'bcpqv5glodad': jung_supporters,
+	'bcpov6zmythd': jung_supporters,
+	'bcplz7zfedam': jung_supporters,
+	'bcpoz4zmiwsk': jung_supporters,
+	'bcpov6krdbf9': jung_supporters,
+	'bcpov6rsgsum': jung_supporters,
+	'bcpov2zzijdh': jung_supporters,
+	'bcpov3zdtlli': jung_supporters,
+	'bcpov6rsreci': jung_supporters,
 }
